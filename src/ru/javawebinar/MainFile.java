50904ede6e5a3586d1af8e082c8822affbbf9df9
secretbottle <Jacjwrath@gmail.com>
2019-12-19 17:49:12 +0300

    HW08 Add print all filenames in project dir

--- src/ru/javawebinar/MainFile.java
+++ src/ru/javawebinar/MainFile.java
@@ -1,33 +1,23 @@
 package ru.javawebinar;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.util.Objects;
 
 public class MainFile {
+    
     public static void main(String[] args) {
-        String filePath = ".\\.gitignore";
+        String ROOT = "./src/ru/javawebinar";
+        File dir = new File(ROOT);
+        outputFileName(Objects.requireNonNull(dir.listFiles(), "Array is null"));
+    }
 
-        File file = new File(filePath);
-        try {
-            System.out.println(file.getCanonicalPath());
-        } catch (IOException e) {
-            throw new RuntimeException("Error", e);
-        }
-
-        File dir = new File("./src/ru/javawebinar/basejava");
-        System.out.println(dir.isDirectory());
-        String[] list = dir.list();
-        if (list != null) {
-            for (String name : list) {
-                System.out.println(name);
+    private static void outputFileName(File[] arr) {
+        for (File f : arr) {
+            if (f.isDirectory()) {
+                outputFileName(Objects.requireNonNull(f.listFiles(), "Array is null"));
+                continue;
             }
-        }
-
-        try (FileInputStream fis = new FileInputStream(filePath)) {
-            System.out.println(fis.read());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println(f.getName());
         }
     }
 }
