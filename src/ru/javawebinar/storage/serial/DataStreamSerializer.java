f271826a2b879e84c687989327c8178358d45e11
secretbottle <Jacjwrath@gmail.com>
2020-01-17 18:02:42 +0300

    HW10 Add DataInput/OutputStream

--- src/ru/javawebinar/storage/serial/DataStreamSerializer.java
+++ src/ru/javawebinar/storage/serial/DataStreamSerializer.java
@@ -4,8 +4,11 @@ import ru.javawebinar.exception.StorageException;
 import ru.javawebinar.model.*;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DataStreamSerializer implements SerializableStrategy {
     @Override
@@ -20,44 +23,32 @@ public class DataStreamSerializer implements SerializableStrategy {
                 dos.writeUTF(entry.getValue());
             }
 
-
             Map<SectionType, Section> sections = resume.getSectionMap();
-            dos.writeInt(sections.size());
-
+            //dos.writeInt(sections.size());
             for (Map.Entry<SectionType, Section> entry : sections.entrySet()) {
-
                 dos.writeUTF(entry.getKey().name());
-
                 Section sectionValue = entry.getValue();
 
                 if (entry.getValue() instanceof TextSection) {
-
                     dos.writeUTF(((TextSection) sectionValue).getText());
-
                     continue;
                 }
 
-
                 if (sectionValue instanceof ListSection) {
-
                     List<String> descriptionList = ((ListSection) sectionValue).getDescriptionList();
                     dos.writeInt(descriptionList.size());
 
                     for (String s : descriptionList) {
                         dos.writeUTF(s);
                     }
-
                     continue;
                 }
 
                 if (sectionValue instanceof OrganizationsSection) {
-
-
                     List<Organization> descriptionList = ((OrganizationsSection) sectionValue).getOrganizations();
                     dos.writeInt(descriptionList.size());
 
                     for (Organization org : descriptionList) {
-
                         dos.writeUTF(org.getLink().getTitle());
                         dos.writeUTF(org.getLink().getUrlAdr());
 
@@ -68,17 +59,14 @@ public class DataStreamSerializer implements SerializableStrategy {
                             dos.writeUTF(pos.getStartPeriod().toString());
                             dos.writeUTF(pos.getEndPeriod().toString());
                             dos.writeUTF(pos.getPosition());
-                            if(pos.getDescription() == null){
-                                dos.writeUTF("null");
+                            if (pos.getDescription() == null) {
+                                dos.writeUTF("");
                             } else {
                                 dos.writeUTF(pos.getDescription());
                             }
-
                         }
-
                     }
                 }
-
             }
 
         } catch (IOException e) {
@@ -97,8 +85,40 @@ public class DataStreamSerializer implements SerializableStrategy {
             for (int i = 0; i < size; i++) {
                 resume.putContactMap(ContactType.valueOf(dis.readUTF()), dis.readUTF());
             }
+
+
+            resume.putSectionMap(SectionType.valueOf(dis.readUTF()), new TextSection(dis.readUTF()));
+            resume.putSectionMap(SectionType.valueOf(dis.readUTF()), new TextSection(dis.readUTF()));
+
+            List<String> descriptionList1 = Stream.of(dis.readUTF()).limit(dis.readInt()).collect(Collectors.toList());
+            resume.putSectionMap(SectionType.valueOf(dis.readUTF()),
+                    new ListSection(descriptionList1));
+
+            List<String> descriptionList2 = Stream.of(dis.readUTF()).limit(dis.readInt()).collect(Collectors.toList());
+            resume.putSectionMap(SectionType.valueOf(dis.readUTF()),
+                    new ListSection(descriptionList2));
+
+
+            resume.putSectionMap(SectionType.valueOf(dis.readUTF()), new OrganizationsSection(dis.readUTF()));
+            resume.putSectionMap(SectionType.valueOf(dis.readUTF()), new OrganizationsSection(dis.readUTF()));
+
+
+            List<Organization> organizations;
+            Link link;
+            List<Organization.Position> organizationsPos;
+            Organization.Position orgPos;
+
+
+            resume.putSectionMap(SectionType.valueOf(dis.readUTF()), dis.readUTF());
+
+
             return resume;
         }
+    }
+
+    private <T> List readToResume(DataInputStream dis, Resume resume){
+        List<T> description = new ArrayList<>();
+        return description;
     }
 
 
